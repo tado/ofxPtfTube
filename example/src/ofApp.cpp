13291415c7f105d371adf52b4643f2695f8a6e06
Atsushi Tadokoro <tadokoro@gmail.com>
2017-08-03 12:14:06 +0900

    update

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -13,15 +13,15 @@ void ofApp::setup(){
     float speed = 0.01;
     for (int i = 0; i < 4000; i++) {
         ofVec3f position;
-        float r = sin(i * speed * 0.2) * 80 + i / 12.0;
+        float r = sin(i * speed * 0.21) * 200.0;
         position.z = sin(i * speed) * r;
-        position.x  = cos(i * speed) * r;
-        position.y  = sin(i * speed * 1.3) * r;
-        float hue = abs(sin(i * speed * 1.3) * 255);
+        position.x  = cos(i * speed * 1.2) * r;
+        position.y  = sin(i * speed * 1.7) * r;
+        float hue = abs(sin(i * speed) * 255);
         ofColor col;
         col.setHsb(hue, 127, 255);
         tube->addSegment(position);
-        tube->addRadius(sin(i * speed * 5.0) * 10.0 + 25);
+        tube->addRadius(sin(i * speed * 3.0) * 12.0 + 26.0);
         tube->addColor(col);
     }
     tube->generate();
@@ -34,8 +34,11 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofBackgroundGradient(ofColor(127), ofColor(91));
     cam.begin();
+    ofEnableDepthTest();
     tube->draw();
+    ofDisableDepthTest();
     cam.end();
 }
 
